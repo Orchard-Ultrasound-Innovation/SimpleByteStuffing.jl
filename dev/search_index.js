cd3933ebed1c139a622ef1688742aa23a589f139
Documenter.jl <documenter@juliadocs.github.io>
2022-07-11 22:11:15 +0000

    build based on 09f108b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SimpleByteStuffing","category":"page"},{"location":"#SimpleByteStuffing","page":"Home","title":"SimpleByteStuffing","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SimpleByteStuffing.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SimpleByteStuffing]","category":"page"}]
+}
