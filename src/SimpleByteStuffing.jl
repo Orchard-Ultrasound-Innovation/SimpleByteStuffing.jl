a4fefa4697630fd4999043bf67cbe456787fc297
LongSnn <long@snnelectronics.com>
2022-07-13 16:04:19 -0700

    Added parse_packet function. Changed Any array type to UInt8

--- src/SimpleByteStuffing.jl
+++ src/SimpleByteStuffing.jl
@@ -10,23 +10,30 @@ read_or_write: acceptables values are READ and WRITE
 payload: vector of 8-bits
 ```
 
+To parse a received packet:
 ```
-packet: [SOP][CMD][R/W][Payload1][...][Payloadn][Checksum][EOP]
+data = parse_packet(rx_packet)
+```
+
+```
+packet: [SOP][CMD][Stat][Payload1][...][Payloadn][Checksum][EOP]
 SOP - start of packet
-CMD - command 
-R/W - read-write byte.  
+CMD - command with read/write bit 
+Status - ACK/NAK, only exists for received packets
 Payload - data
 Checksum - 8-bit checksum
 EOP - end of packet
 ```
+
 """
 module SimpleByteStuffing
 
-const READ = 0x00
-const WRITE = 0x01
+const READ = 0x01
+const WRITE = 0x00
 
 export READ, WRITE
 export create_packet
+export parse_packet
 
 include("bytestuffing.jl")
 
--- src/bytestuffing.jl
+++ src/bytestuffing.jl
@@ -1,22 +1,30 @@
+const ACK = 0xFA
+const NAK = 0xFB
 const SOP = 0xFC
 const EOP = 0xFD
 const ESC = 0xFE
 
 const FLAG = [SOP, EOP, ESC]
 
-function create_packet(command::UInt8, read_or_write::UInt8; payload=[])
+function create_packet(command::UInt8, read_or_write; payload::Vector{UInt8}=UInt8[])
+    verify_read_or_write(read_or_write)
     packet = []
     push!(packet, SOP)
-    push!(packet, command)
-    verify_read_or_write(read_or_write)
-    push!(packet, read_or_write)
-    !isempty(payload) && append!(packet, get_byte_stuffed_payload(payload) )
-    push!(packet, get_checksum(command, read_or_write, payload))
+    push!(packet, command | (read_or_write<<7) )
+    !isempty(payload) && append!(packet, get_byte_stuffed_payload(payload))
+    data = combine(command, read_or_write, payload)
+    push!(packet, get_checksum(data))
     push!(packet, EOP)
     return convert(Vector{UInt8}, packet)
 end
 
-verify_read_or_write(read_or_write) = !(read_or_write in [READ,WRITE]) && error("read_or_write must be of value READ:0x00 or WRITE:0x01.")
+verify_read_or_write(read_or_write) = !(read_or_write in [READ,WRITE]) && error("read_or_write must be of value READ or WRITE.")
+
+function combine(command, read_or_write, payload)
+    data = [command, read_or_write]
+    !isempty(payload) && append!(data, payload)
+    return convert(Vector{UInt8}, data)
+end
 
 function get_byte_stuffed_payload(payload::Vector{UInt8})
 
@@ -33,12 +41,54 @@ function get_byte_stuffed_payload(payload::Vector{UInt8})
     return bs_payload
 end
 
-function get_checksum(command, read_or_write, payload)
-    checksum = [command, read_or_write]
-    !isempty(payload) && append!(checksum, payload)
-    checksum = convert(Vector{UInt8}, checksum)
-    checksum = sum(checksum)
+function parse_packet(packet)
+    
+    sop = findfirst(x->x==SOP, packet)
+    verify_sop(sop)
+
+    ack = packet[sop+2]
+    verify_ack(ack)
+
+    eop = findfirst(x->x==EOP, packet)
+    verify_eop(eop)
+
+    data = packet[sop+1 : eop-2]
+    destuffed = destuff_data(data)
+    crc = get_checksum(data)
+    verify_checksum( packet[eop-1], crc)
+    
+    return destuffed[3:end]
+end
+
+verify_sop(sop) = isempty(sop) && error("Packet is invalid! Start-of-packet not found.")
+verify_ack(ack) = (ack != ACK) && error("Packet is invalid! ACK not found.")
+verify_eop(eop) = isempty(eop) && error("Packet is invalid! End-of-packet not found.")
+verify_checksum(sum1, sum2) = (sum1 != sum2) && error("Packet is invalid! CRC: does not match.")
+
+function destuff_data(bs_data)
+
+    len = length(bs_data)
+    i = 1
+    data = []
+
+    while(i <= len)
+        if bs_data[i] == ESC
+            i = i+1
+            push!(data, xor( bs_data[i] ,0x20))
+        else
+            push!(data, bs_data[i])
+        end
+
+        i = i + 1
+    end    
+
+    return data
+
+end
+
+function get_checksum(data)
+    checksum = sum(data)
     checksum = UInt8( checksum%256 )
     checksum = ~checksum
-    return checksum
+    return UInt8(checksum)
 end
